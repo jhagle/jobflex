003e10181f43d26df248b4f2c24da307168f21ea
James Hagle <rob.hagle@gmail.com>
2015-05-27 15:21:53 -0500

    Commiting jareds changes
    Also reverted by mistake, please ignore

--- config/passport.js
+++ config/passport.js
@@ -111,3 +111,37 @@ module.exports = function(passport) {
         }));
 
 };
+
+//Test for registion tabs
+$(function() {
+
+    $('#login-form-link').click(function(e) {
+        $("#login-form").delay(100).fadeIn(100);
+        $("#candidate-register-form").fadeOut(100);
+        $("#company-register-form").fadeOut(100);
+        $('#candidate-register-form-link').removeClass('active');
+        $('#company-register-form-link').removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
+    });
+    $('#candidate-register-form-link').click(function(e) {
+        $("#candidate-register-form").delay(100).fadeIn(100);
+        $("#login-form").fadeOut(100);
+        $("#company-register-form").fadeOut(100);
+        $('#login-form-link').removeClass('active');
+        $('#company-register-form-link').removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
+    });
+    $('#company-register-form-link').click(function(e) {
+        $("#company-register-form").delay(100).fadeIn(100);
+        $("#login-form").fadeOut(100);
+        $("#candidate-register-form").fadeOut(100);
+        $('#login-form-link').removeClass('active');
+        $('#candidate-register-form-link').removeClass('active');
+
+        $(this).addClass('active');
+        e.preventDefault();
+    });
+
+});
