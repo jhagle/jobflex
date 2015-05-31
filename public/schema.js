3a49d90e317a08e3b38c175cbace3f5522b9e0bc
sdweber422 <sdweber422@gmail.com>
2015-05-30 20:54:44 -0500

    Added variables and methods.
    Added variables and methods for jobListing.json information

--- public/schema.js
+++ public/schema.js
@@ -8,14 +8,17 @@
     var fs = require('fs');
     var candidateContent = String(fs.readFileSync('./candidateData.json','UTF-8'));
     var companyContent = String(fs.readFileSync('./companyData.json','UTF-8'));
-    var candidateData = JSON.parse(candidateContent) ;
+    var jobContent = String(fs.readFileSync('./jobListing.json','UTF-8'));
+    var candidateData = JSON.parse(candidateContent);
     var companyData = JSON.parse(companyContent);
+    var jobData = JSON.parse(jobContent);
     var candidateLength = candidateData.length;
     var companyLength = companyData.length;
+    var jobLength = jobData.length;
 
 //Connect to database. Database 'jobflex' is automatically created if it does not previously exist.
 
-mongoose.connect('mongodb://localhost/jobflex')
+mongoose.connect('mongodb://admin:admin@proximus.modulusmongo.net:27017/uxaz8uZi');
 
 /*Test connection*/
 
@@ -31,7 +34,7 @@ db.once('open', function (callback) {
 
 var candidateSchema = mongoose.Schema(
     {
-        username : String,
+        email: {type: String, required: true, unique: true},
         password : String,
         firstName : String,
         lastName : String,
@@ -42,6 +45,8 @@ var candidateSchema = mongoose.Schema(
         state : String,
         created : {type: Date, default: Date.now},
         webpage: String,
+        pic: Buffer,
+        major: String,
         resume: Buffer,
         references: mongoose.Schema.Types.Mixed,
         skillsSort: Array,
@@ -52,7 +57,7 @@ var candidateSchema = mongoose.Schema(
 
 var companySchema = mongoose.Schema(
     {
-        companyName : String,
+        companyName : {type: String, required: true, unique: true},
         password: String,
         corporateId : Number,
         firstName : String,
@@ -63,10 +68,21 @@ var companySchema = mongoose.Schema(
         email: String,
         created : {type: Date, default: Date.now},
         webpage: String,
+        logo: Buffer,
         description: String,
-        skillsSort: Array,
-        workAttitude: Array
+            workAttitude: Array
+    }
+)
 
+var jobSchema = mongoose.Schema(
+    {
+        companyName: String,
+        title: String,
+        jobDescription: String,
+        city: String,
+        state: String,
+        datePosted: {type: Date, default: Date.now},
+        skillsSort: Array
     }
 )
 
@@ -75,6 +91,8 @@ var companySchema = mongoose.Schema(
 
 var candidate = mongoose.model('candidate', candidateSchema)
 var company = mongoose.model('company', companySchema)
+var job = mongoose.model('job', jobSchema)
+
 
 //Populate candidate collection if there is data in companyData.json
 
@@ -94,6 +112,14 @@ var company = mongoose.model('company', companySchema)
     for (var i = 0; i < companyLength; i++) {
         var populateCompany = new company(companyData[i]);
         populateCompany.save(function (err, data) {
+            if (err) console.log(err);
+            else console.log('Saved : ', data);
+        });
+    };
+
+    for (var i = 0; i < jobLength; i++) {
+        var populateJob = new job(jobData[i]);
+        populateJob.save(function (err, data) {
             if (err) console.log(err);
             else console.log('Saved : ', data);
         });
