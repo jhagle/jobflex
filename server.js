3fa3fdfa2467d3aa1f50d0eab92e2ae572d19bd3
James Hagle <rob.hagle@gmail.com>
2015-06-10 15:35:43 -0500

    Added views file to app for static files

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secre
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
-
+app.use("/views", express.static(__dirname + '/views'));
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
